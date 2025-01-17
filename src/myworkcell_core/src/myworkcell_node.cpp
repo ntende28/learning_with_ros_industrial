0e9bbfba15da03b20ad52baba0340a3fe55bc650
movertraining <ntende28>
2025-01-17 19:21:19 +0800

    starting urdf and stuff

--- src/myworkcell_core/src/myworkcell_node.cpp
+++ src/myworkcell_core/src/myworkcell_node.cpp
@@ -10,7 +10,7 @@ public:
         vision_client_ = nh.serviceClient<myworkcell_core::LocalizePart>("localize_part");
     }
 
-    void start() 
+    void start(const std::string& base_frame) 
     {
         ROS_INFO("Attempting to localize part");
         // Localize the part
@@ -21,6 +21,9 @@ public:
             return;
         }
         ROS_INFO_STREAM("part localized: " << srv.response);
+        
+        srv.request.base_frame = base_frame;
+        ROS_INFO_STREAM("Requesting pose in base frame: " << base_frame); 
     }
 
 private:
@@ -33,12 +36,20 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "myworkcell_node");
     ros::NodeHandle nh;
+
+    // adding a private node handle
+    ros::NodeHandle private_node_handle ("~");
+
+    // parameter name, string object reference, default value
+    std::string base_frame;
+    private_node_handle.param<std::string>("base_frame", base_frame, "world"); 
+
     ROS_INFO("ScanNPlan node has been initialized");
 
     ScanNPlan app(nh);
 
     ros::Duration(.5).sleep();  // wait for the class to initialize
-    app.start();
+    app.start(base_frame);
 
     ros::spin();
 }
